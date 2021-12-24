0a263ed5198b55202b9720bc2cc442556b87b9b4
Baran Kurtbogan <barankurtbogan@gmail.com>
2021-12-24 08:25:36 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/MyBlogContext.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class MyBlogContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=MyBlogDB;Trusted_Connection=true");
+        }
+
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+    }
+}
